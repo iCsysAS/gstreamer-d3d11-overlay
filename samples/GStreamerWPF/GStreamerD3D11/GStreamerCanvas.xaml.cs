a3cf9540764f029b7057802dd400373cf8959cda
Andreas Skaar <ask@icsys.no>
2023-04-21 15:35:20 +0200

    Handle DisplaySettingsChanged and loss of image

--- samples/GStreamerWPF/GStreamerD3D11/GStreamerCanvas.xaml.cs
+++ samples/GStreamerWPF/GStreamerD3D11/GStreamerCanvas.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using D3D11Scene;
 using Gst;
 using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
@@ -13,6 +14,7 @@ namespace GStreamerD3D11
 
         private D3DImageEx _D3DImageEx;
         private D3D11TestScene _D3D11Scene;
+        private object _lockObject = new object();
 
 
         public static DependencyProperty EnabledProperty = DependencyProperty.Register("Enabled", typeof(bool), typeof(GStreamerCanvas), new PropertyMetadata(false, new PropertyChangedCallback(EnabledPropertyChanged)));
@@ -35,6 +37,13 @@ namespace GStreamerD3D11
             Loaded += Control_Loaded;
             Unloaded += Control_Unloaded;
             Microsoft.Win32.SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+            Microsoft.Win32.SystemEvents.DisplaySettingsChanged += SystemEvents_DisplaySettingsChanged;
+        }
+
+        private void SystemEvents_DisplaySettingsChanged(object? sender, EventArgs e)
+        {
+            DisposeScene();
+            Reload();
         }
 
         private void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
@@ -42,10 +51,12 @@ namespace GStreamerD3D11
             if (e.Reason == Microsoft.Win32.SessionSwitchReason.SessionUnlock ||
                 e.Reason == Microsoft.Win32.SessionSwitchReason.SessionLogon)
             {
+                Console.WriteLine("SystemEvents_SessionSwitch Reload");
                 Reload();
             }
             else
             {
+                Console.WriteLine("SystemEvents_SessionSwitch dispose");
                 DisposeScene();
             }
         }
@@ -62,25 +73,38 @@ namespace GStreamerD3D11
 
         private void Reload()
         {
-            if (!Enabled)
+            lock (_lockObject)
             {
-                return;
+                if (!Enabled)
+                {
+                    return;
+                }
+
+                try
+                {
+                    _D3DImageEx = new D3DImageEx();
+                    _D3D11Scene = new D3D11TestScene(1920, 1080);
+
+                    d3dScene.Source = _D3DImageEx;
+
+                    /* Set the backbuffer, which is a ID3D11Texture2D pointer */
+                    var renderTarget = _D3D11Scene.GetRenderTarget();
+                    var backBuffer = _D3DImageEx.CreateBackBuffer(D3DResourceTypeEx.ID3D11Texture2D, renderTarget);
+                    var enableSoftwareFallback = false;
+                    _D3DImageEx.Lock();
+                    _D3DImageEx.SetBackBuffer(D3DResourceType.IDirect3DSurface9, backBuffer, enableSoftwareFallback);
+                    _D3DImageEx.Unlock();
+
+                    CompositionTarget.Rendering += CompositionTarget_Rendering;
+                }
+                catch (Exception ex)
+                {
+                    DisposeScene();
+                    Enabled = false;
+                    txtError.Visibility = Visibility.Visible;
+                    txtError.Text = ex.ToString();
+                }
             }
-
-            _D3DImageEx = new D3DImageEx();
-            _D3D11Scene = new D3D11TestScene(1920, 1080);
-
-            d3dScene.Source = _D3DImageEx;
-
-            /* Set the backbuffer, which is a ID3D11Texture2D pointer */
-            var renderTarget = _D3D11Scene.GetRenderTarget();
-            var backBuffer = _D3DImageEx.CreateBackBuffer(D3DResourceTypeEx.ID3D11Texture2D, renderTarget);
-            var enableSoftwareFallback = false;
-            _D3DImageEx.Lock();
-            _D3DImageEx.SetBackBuffer(D3DResourceType.IDirect3DSurface9, backBuffer, enableSoftwareFallback);
-            _D3DImageEx.Unlock();
-
-            CompositionTarget.Rendering += CompositionTarget_Rendering;
         }
 
         public void BeginDraw(Element sink, GLib.SignalArgs args)
@@ -99,31 +123,40 @@ namespace GStreamerD3D11
 
         private void InvalidateD3DImage()
         {
-            _D3DImageEx.Lock();
-
-            _D3DImageEx.AddDirtyRect(new Int32Rect()
+            if (_D3DImageEx != null)
             {
-                X = 0,
-                Y = 0,
-                Height = _D3DImageEx.PixelHeight,
-                Width = _D3DImageEx.PixelWidth
-            });
+                _D3DImageEx.Lock();
 
-            _D3DImageEx.Unlock();
+                _D3DImageEx.AddDirtyRect(new Int32Rect()
+                {
+                    X = 0,
+                    Y = 0,
+                    Height = _D3DImageEx.PixelHeight,
+                    Width = _D3DImageEx.PixelWidth
+                });
+
+                _D3DImageEx.Unlock();
+            }
         }
 
 
         public void Dispose()
         {
             Microsoft.Win32.SystemEvents.SessionSwitch -= SystemEvents_SessionSwitch;
+            Microsoft.Win32.SystemEvents.DisplaySettingsChanged -= SystemEvents_DisplaySettingsChanged;
             DisposeScene();
         }
 
         private void DisposeScene()
         {
-            if (_D3DImageEx != null)
+            lock (_lockObject)
             {
-                _D3D11Scene.Dispose();
+                if (_D3DImageEx != null)
+                {
+                    _D3D11Scene.Dispose();
+                    _D3D11Scene = null;
+                    _D3DImageEx = null;
+                }
             }
         }
     }
